1ff10bdf2df54f31c9ef49d95c4e1d357e4a9b2e
OlMar <OlMar@OlMar-VAIO>
2012-11-17 08:52:45 +0100

    Fixed Pat_history structure - added Doctor reference

--- Mpr_clinic/sql/pat_history.sql
+++ Mpr_clinic/sql/pat_history.sql
@@ -1,9 +1,12 @@
 CREATE TABLE `pat_history` (
   `Hst_id` int(11) NOT NULL,
   `Hst_Pat_id` int(11) NOT NULL,
+  `Hst_Doc_id` int(11)	NOT NULL,
   `Hst_diagnosis` varchar(1000) NOT NULL,
   `Hst_prescription` varchar(200) NOT NULL,
   PRIMARY KEY (`Hst_id`),
   KEY `fk_Hst_Pat_id_idx` (`Hst_Pat_id`),
   CONSTRAINT `fk_Hst_Pat_id` FOREIGN KEY (`Hst_Pat_id`) REFERENCES `patient` (`Pat_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `fk_Hst_Doc_id_idx` (`Hst_Doc_id`),
+  CONSTRAINT `fk_Hst_Doc_id` FOREIGN KEY (`Hst_Doc_id`) REFERENCES `doctor` (`Doc_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 )
